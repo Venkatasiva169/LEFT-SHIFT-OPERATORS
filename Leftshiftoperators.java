03d532dee3f222b16eacc351012b8e897e25ac75
Venkatasiva169 <Sivachaganti16@gmail.com>
2025-01-28 10:23:18 +0530

    Add files via upload

---
+++ Leftshiftoperators.java
@@ -0,0 +1,16 @@
+public class Leftshiftoperators {
+        public static void main(String[] args) {
+            int num1 = 4;  
+            int num2 = -4; 
+    
+            
+            int result1 = num1 << 2;  
+            System.out.println("Result of " + num1 + " << 2 = " + result1);
+    
+            
+            int result2 = num2 << 2;  
+            System.out.println("Result of " + num2 + " << 2 = " + result2);
+        }
+    }
+    
+
